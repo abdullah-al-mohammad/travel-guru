fb97b2cf950c5e34a948957c49d0d836ae96d141
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-08-23 11:18:53 +0600

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../navbar/Navbar";
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="bgImg">
+            <Navbar></Navbar>
+            <h2>this is home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import logo from '../../assets/logo.png';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 bg-transparent">
+            <img className='w-20' src={logo} alt="" />
+
+            <div className="navbar-end">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-start">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                            <path
+                                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                        </svg>
+                        <input type="text" className="grow" placeholder="Email" />
+                    </label>
+                </div>
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
