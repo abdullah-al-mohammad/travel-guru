45b9eb45a5b0b8ba8794e348094773334a6a2ce8
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-08-24 09:24:36 +0600

    add home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,43 @@ import './Home.css'
 const Home = () => {
     return (
         <div className="bgImg">
-            <Navbar></Navbar>
-            <h2>this is home</h2>
+            <div className="container mx-auto">
+                <Navbar></Navbar>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-3 items-center">
+                    <div className="">
+                        <h1 className="text-6xl font-bebasNeue">Cox's bazar</h1>
+                        <p>Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.</p>
+                    </div>
+                    <div>
+                        <div className="min-h-screen">
+                            <div className="hero-content flex-col lg:flex-row-reverse">
+                                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                                    <form className="card-body">
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Email</span>
+                                            </label>
+                                            <input type="email" placeholder="email" className="input input-bordered" required />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Password</span>
+                                            </label>
+                                            <input type="password" placeholder="password" className="input input-bordered" required />
+                                            <label className="label">
+                                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                            </label>
+                                        </div>
+                                        <div className="form-control mt-6">
+                                            <button className="btn btn-primary">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
+    const links = <>
+        <li><NavLink to='/news'>News</NavLink></li>
+        <li><NavLink to='/destination'>Destination</NavLink></li>
+        <li><NavLink to='/blog'>Blog</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+        <li><NavLink className='btn btn-warning px-6' to='/login'>Login</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100 bg-transparent">
-            <img className='w-20' src={logo} alt="" />
-
+            <img className='w-20 text-white' src={logo} alt="" />
             <div className="navbar-end">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,46 +32,21 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {links}
                     </ul>
                 </div>
                 <div className="navbar-start">
-                    <label className="input input-bordered flex items-center gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                            <path
-                                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                        </svg>
-                        <input type="text" className="grow" placeholder="Email" />
+                    <label className="input input-bordered flex items-center gap-2 opacity-60 border-white">
+                        <FaSearch></FaSearch>
+                        <input type="text" className="grow" placeholder="Search your Destination..." />
                     </label>
                 </div>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {
+                        links
+                    }
                 </ul>
             </div>
 
